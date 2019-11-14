d00bca1b56d90128792433263c8bc002ad486e16
Jarbi Bonilla <jarbibonilla@gmail.com>
2019-11-14 06:22:35 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -16,11 +16,9 @@ class Triangle
       begin
         raise TriangleError
       end 
-    elsif
-       side1 == side2 && side3 == side2 && side1 == side3
+    elsif side1 == side2 && side3 == side2 && side1 == side3
         :equilateral
-    elsif
-       side1 == side2 || side1 == side3 || side2 == side3
+    elsif side1 == side2 || side1 == side3 || side2 == side3
         :isosceles
     else
        :scalene
